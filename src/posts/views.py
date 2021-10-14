29d78ddee8fabc18c002f050c1a7afd605455d34
Pablo de la Cuesta <pablodelacuesta@hotmail.es>
2021-10-14 18:03:03 +0200

    added news letter

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,10 +1,21 @@
 from django.db.models import query
 from django.shortcuts import render
 from .models import Post
+from mail.models import Signup
 
 def index(request):
     featured = Post.objects.filter(featured=True)
     latests = Post.objects.order_by('-timestamp')[0:3]
+
+    if request.method == 'POST':
+        
+        # TODO: We need validation
+
+        email = request.POST['email']
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+
     context = {
         'object_list': featured,
         'object_latest': latests
